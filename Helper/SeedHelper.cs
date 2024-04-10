ce4fbc90eb840258760b534d70428910bd2ded63
PeterSahlin <145502083+PeterSahlin@users.noreply.github.com>
2024-04-10 15:53:24 +0200

    Added seed for Categories

---
+++ Helper/SeedHelper.cs
@@ -0,0 +1,27 @@
+﻿using FribergHomez.Data;
+using FribergHomez.Models;
+
+namespace FribergHomez.Helper
+{
+    public static class SeedHelper
+    {
+        public static void SeedCategories (ApplicationDbContext applicationDbContext)
+        {
+            if (!applicationDbContext.Categories.Any())
+            {
+                List<Category> categoryList = new List<Category>
+                {
+                    new Category("House"),
+                    new Category("Condo"),
+                    new Category("Cottage"),
+                    new Category("Condo Terraced House"),
+
+                };
+                    applicationDbContext.AddRange(categoryList);
+                    applicationDbContext.SaveChanges();
+
+            }
+        }
+
+    }
+}
