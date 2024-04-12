e9488b5d0a29d5e983dc7861f6a98005c6009547
Nujsk <111609565+Nujsk@users.noreply.github.com>
2024-04-12 10:43:00 +0200

    Added repo and interface for Municipality & SaleObject

--- Data/IMunicipality.cs
+++ Data/IMunicipality.cs
@@ -1,7 +1,14 @@
-﻿namespace FribergHomez.Data
+﻿using FribergHomez.Models;
+
+namespace FribergHomez.Data
 {
     //Henrik
     public interface IMunicipality
     {
+        Task<List<Municipality>> GetAllMunicipalitiesAsync();
+        Task GetMunicipalityByIdAsync(int id);
+        Task AddMunicipalityAsync(Municipality municipality);
+        Task DeleteMunicipalityAsync(int id);
+        Task UpdateMunicipalityAsync(Municipality municipality);
     }
 }
