5a0395e01a01078d0c5a91365c462358be91a567
Thomas Sahlin <thomassahlin@me.com>
2024-04-19 11:10:51 +0200

    Identity

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,10 +1,13 @@
 ﻿using FribergHomez.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace FribergHomez.Data
 {
     //All
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext<RealEstateAgent, IdentityRole<Guid>, Guid>
     {
 
         public DbSet<Firm> Firms { get; set; }
