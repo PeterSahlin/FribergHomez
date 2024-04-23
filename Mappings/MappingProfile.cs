a4196c328e1f76d25fadbf9096b0fc189e0565c1
PeterSahlin <145502083+PeterSahlin@users.noreply.github.com>
2024-04-23 15:21:31 +0200

    Changed property names for DTO

--- Mappings/MappingProfile.cs
+++ Mappings/MappingProfile.cs
@@ -10,8 +10,8 @@ namespace FribergHomez.Mappings
         {
             //SaleObject
             CreateMap<SaleObject, SalesObjectDto>()
-            .ForMember(sODto => sODto.ConstructionYear, opt => opt.MapFrom(s => s.YearOfConstruction))
-            .ForMember(sODto => sODto.AnnualOperatingCost, opt => opt.MapFrom(s => s.OperatingCostPerYear))
+            //.ForMember(sODto => sODto.ConstructionYear, opt => opt.MapFrom(s => s.YearOfConstruction))
+            //.ForMember(sODto => sODto.AnnualOperatingCost, opt => opt.MapFrom(s => s.OperatingCostPerYear))
             .ReverseMap();
 
             //RealEstateAgent
