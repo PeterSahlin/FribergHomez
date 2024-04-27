3c49cd052a6991bd5248d458986466a043a938cd
Nujsk <111609565+Nujsk@users.noreply.github.com>
2024-04-27 21:59:07 +0200

    Updated Put method for Category

--- Migrations/20240426092329_init.Designer.cs
+++ Migrations/20240426092329_init.Designer.cs
@@ -12,8 +12,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FribergHomez.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20240426084041_added isactive to saleobject2")]
-    partial class addedisactivetosaleobject2
+    [Migration("20240426092329_init")]
+    partial class init
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
